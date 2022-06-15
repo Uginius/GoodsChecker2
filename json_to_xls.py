cfd05db75b2a17cd61d2fc8ca68f4400144fe0cd
Uginius <e.slastin@gmail.com>
2022-06-15 17:29:36 +0300

    check_empty_platform_ratings added

--- json_to_xls.py
+++ json_to_xls.py
@@ -3,6 +3,7 @@ import math
 from openpyxl import Workbook
 from openpyxl.styles import PatternFill, Alignment, Font
 
+from parsers.parse_rating_votonia import VotoniaRatingUpdate
 from utilites import check_dir
 
 
@@ -27,15 +28,19 @@ class XlsConverter:
     def combine_data_to_write(self):
         sw = self.workbook.active
         for shop in self.goods_dict:
+            self.check_empty_platform_ratings(shop)
             shop_goods = self.goods_dict[shop]
             for shop_id in shop_goods:
                 merch = shop_goods[shop_id]
                 qt = merch['vote_qt']
                 rating = merch['vote_rating']
-                if rating > 4 and qt > 1:
-                    need = 0
+                if rating is None:
+                    need = None
                 else:
-                    need = math.ceil(qt * abs(4 - rating)) if rating else 2
+                    if rating > 4 and qt > 1:
+                        need = 0
+                    else:
+                        need = math.ceil(qt * abs(4 - rating)) if rating else 2
                 data = [shop, int(shop_id), merch['brand'], merch['name'], merch['url'], merch['status'],
                         merch['price'], rating, qt, need]
                 sw.append(data)
@@ -46,9 +51,10 @@ class XlsConverter:
                  'price', 'rating', 'votes', 'need']
         ws = self.workbook.active
         ws.append(title)
+        ws.column_dimensions['B'].width = 12
         ws.column_dimensions['D'].width = 40
         ws.column_dimensions['E'].width = 30
-        ws.column_dimensions['F'].width = 25
+        ws.column_dimensions['F'].width = 15
         for cell in ws[1]:
             cell.fill = PatternFill("solid", fgColor='b3b3b3')
             cell.alignment = Alignment(horizontal='center', vertical='center')
@@ -64,9 +70,20 @@ class XlsConverter:
         rating_cell.alignment = Alignment(horizontal='center', vertical='center')
         votes_cell.alignment = Alignment(horizontal='center', vertical='center')
         need_votes_cell.alignment = Alignment(horizontal='center', vertical='center')
-        if rating_cell.value == 0:
-            rating_cell.fill = PatternFill("solid", fgColor='ffe4e1')
-        elif rating_cell.value < 4:
-            rating_cell.fill = PatternFill("solid", fgColor='E6B8B7')
-        if need_votes_cell.value > 0:
-            need_votes_cell.fill = PatternFill("solid", fgColor='ffcd75')
+        if rating_cell.value:
+            if rating_cell.value == 0:
+                rating_cell.fill = PatternFill("solid", fgColor='ffe4e1')
+            elif rating_cell.value < 4:
+                rating_cell.fill = PatternFill("solid", fgColor='E6B8B7')
+        if need_votes_cell.value:
+            if need_votes_cell.value > 0:
+                need_votes_cell.fill = PatternFill("solid", fgColor='ffcd75')
+
+    def check_empty_platform_ratings(self, shop):
+        match shop:
+            case 'votonia':
+                votonia_goods_updater = VotoniaRatingUpdate(self.goods_dict[shop])
+                votonia_goods_updater.run()
+                self.goods_dict[shop] = votonia_goods_updater.out_goods_data()
+            case 'maxidom':
+                pass
