555fbe3c24cd2ad68e112f5f50e796f2c6bbc278
Uginius <e.slastin@gmail.com>
2022-06-15 18:42:50 +0300

    votonia done

--- parsers/parse_rating_votonia.py
+++ parsers/parse_rating_votonia.py
@@ -1,12 +1,8 @@
 import time
-
 from bs4 import BeautifulSoup
-
 from config import selenium_arguments, browser_path, wait_time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-
-from utilites import time_track
 
 
 class VotoniaRatingUpdate:
@@ -29,13 +25,13 @@ class VotoniaRatingUpdate:
     def out_goods_data(self):
         return self.goods
 
-    @time_track
     def get_products(self):
         goods = self.goods
         for shop_id in goods:
             merch = goods[shop_id]
-            url = merch['url'] + '#tab-review'
-            self.browser.get(url=url)
+            if merch['vote_qt']:
+                return
+            self.browser.get(url=merch['url'])
             time.sleep(2)
             soup = BeautifulSoup(self.browser.page_source, 'lxml').find('div', id='pj_product_info')
             try:
@@ -46,4 +42,3 @@ class VotoniaRatingUpdate:
                 merch['vote_qt'] = int(soup.find('a', attrs={'href': '#tab-review'}).text.split()[1].strip('()'))
             except AttributeError:
                 merch['vote_qt'] = None
-            pass
