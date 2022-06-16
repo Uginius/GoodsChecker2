bee24c2496ed8bed753b91827f28ac12faaceace
Uginius <e.slastin@gmail.com>
2022-06-16 14:40:06 +0300

    add platforms

--- parsers/platform_parsers.py
+++ parsers/platform_parsers.py
@@ -21,18 +21,26 @@ class FatherPlatformParser:
             self.parse_goods_list()
 
     def get_goods_list(self):
+        soup = self.soup
         match self.shop:
+            case 'akson':
+                html_goods = soup.find('div', class_='goods-list__content')
+                self.goods_list = html_goods.find_all('div', class_='product-matrix goods-list__matrix')
             case 'baucenter':
-                catalog = self.soup.find('div', class_='catalog-list')
+                catalog = soup.find('div', class_='catalog-list')
                 self.goods_list = catalog.find_all('div', class_='catalog_item with-tooltip')
             case 'dns':
-                self.goods_list = self.soup.find_all('div', attrs={'data-id': 'product'})
+                self.goods_list = soup.find_all('div', attrs={'data-id': 'product'})
             case 'sdvor':
-                self.goods_list = self.soup.find_all('sd-product-grid-item', class_='product-grid-item')
+                self.goods_list = soup.find_all('sd-product-grid-item', class_='product-grid-item')
+            case 'maxidom':
+                self.goods_list = soup.find('div', class_='item-list-inner').find_all('article')
+            case 'votonia':
+                self.goods_list = self.soup.find_all('div', class_='wfloat cat_product_box is-product')
 
     def parse_goods_list(self):
-        for self.html_product in self.goods_list:
-            cp = parse_platform_product(platform=self.shop, html_product=self.html_product)
+        for html_product in self.goods_list:
+            cp = parse_platform_product(platform=self.shop, html_product=html_product)
             if not cp:
                 continue
             if not cp.trade_mark:
