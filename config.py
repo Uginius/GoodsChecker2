e3b662558b49d5c7f78a5be92f96884f782ad6e8
Uginius <e.slastin@gmail.com>
2022-06-24 12:24:51 +0300

    user-agent in config updated

--- config.py
+++ config.py
@@ -21,28 +21,21 @@ blocklist = {
 }
 
 search_phrases = ['фотон', 'контакт', 'рекорд', 'safeline']
-headers = {
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'accept-encoding': 'gzip, deflate, br',
-    'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36'
-}
-selenium_arguments = [
-    'user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36',
-    '--disable-blink-features=AutomationControlled'
-]
 
 match sys.platform:
     case 'linux':
         browser_path = 'drivers/chromedriver_linux64_99.0.4844.51/chromedriver'
+        user_agent = None
     case 'darwin':
         browser_path = 'drivers/chromedriver'
+        user_agent = None
     case 'win32':
         browser_path = 'drivers/chromedriver.exe'
+        user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'
     case _:
         print("ERROR: can't found selenium driver")
+        user_agent = None
 
-req_headers = {
-    'accept': '*/*',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'
-}
+selenium_arguments = [f'user-agent={user_agent}', '--disable-blink-features=AutomationControlled']
+req_headers = {'accept': '*/*', 'accept-encoding': 'gzip, deflate, br',
+               'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7', 'user-agent': user_agent}
